fe8372b3d26e94ab06f7175a7649d370b37863b2
Jayson Stemmler <jstemm@uw.edu>
2015-01-14 12:06:12 -0800

    update the example directory with some sample files and better sample scripts

--- examples/sfcmet_example.py
+++ examples/sfcmet_example.py
@@ -1,4 +1,7 @@
 import pyadapt
-f = '/home/jstemm/Documents/research/data/sfcmet/grwmetM1.b1.20100531.000000.cdf'
+import os
+
+f = os.path.join('./data/grwmetM1.b1.20100531.000000.cdf')
+
 SFC = pyadapt.read(f)
 SFC.plot(plot_output=True)
--- examples/snd_example.py
+++ examples/snd_example.py
@@ -1,4 +1,7 @@
 import pyadapt
-f = '/Users/jstemm/Documents/research/data/test/grwsondewnpnM1.b1.20100531.112900.cdf'
+import os
+
+f = os.path.join('./data/grwsondewnpnM1.b1.20100531.112900.cdf')
+
 snd = pyadapt.read(f)
-snd.plot(ptop=100, plot_output=True, autoname=True, skew=90, out_dir = '/Users/jstemm/Documents/pyadapt/examples/')
+snd.plot(ptop=100, plot_output=True, autoname=True, skew=90)
