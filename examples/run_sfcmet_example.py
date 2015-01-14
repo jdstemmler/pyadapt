9d04d6356e16587f64ba309ecc7689c036030874
Jayson Stemmler <jstemm@uw.edu>
2015-01-14 14:53:25 -0800

    further cleanup of examples

--- examples/run_sfcmet_example.py
+++ examples/run_sfcmet_example.py
@@ -1,7 +1,8 @@
 import pyadapt
 import os
 
-f = os.path.join('./data/grwmetM1.b1.20100531.000000.cdf')
+file_in = os.path.join('./data/grwmetM1.b1.20100531.000000.cdf')
+plot_dir = os.path.join('./plots/')
 
-SFC = pyadapt.read(f)
-SFC.plot(plot_output=True)
+SFC = pyadapt.read(file_in)
+SFC.plot(plot_output=True, out_dir=plot_dir)
--- examples/run_sounding_example.py
+++ examples/run_sounding_example.py
@@ -1,7 +1,12 @@
 import pyadapt
 import os
 
-f = os.path.join('./data/grwsondewnpnM1.b1.20100531.112900.cdf')
+file_in = os.path.join('./data/grwsondewnpnM1.b1.20100531.112900.cdf')
+plot_dir = os.path.join('./plots/')
 
-snd = pyadapt.read(f)
-snd.plot(ptop=100, plot_output=True, autoname=True, skew=90)
+snd = pyadapt.read(file_in)
+snd.plot(ptop=100,
+         plot_output=True,
+         out_dir = plot_dir,
+         autoname=True,
+         skew=90)
