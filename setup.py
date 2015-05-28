0166e465e5454c6f8b7ebd0c0dea74d6995574f5
Jayson Stemmler <jstemm@uw.edu>
2015-05-28 05:56:39 -0700

    switched from distutils.core to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pyadapt',
       version='0.1.0',
@@ -6,5 +6,5 @@ setup(name='pyadapt',
       author='Jayson Stemmler',
       author_email='jstemm@uw.edu',
       url='https://github.com/jstemmler/pyadapt',
-      packages=['pyadapt','pyadapt.extras', 'pyadapt.datastreams'],
+      packages=['pyadapt'],
      )
