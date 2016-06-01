17c0cc8358bc4ed5fc9e2906e7575c0636574565
Jayson Stemmler <jdstemmler@gmail.com>
2016-06-01 14:47:53 -0700

    add License and setup information

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='PyADAPT',
+    version='0.2.0',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    author='Jayson Stemmler',
+    author_email='jdstemmler@gmail.com',
+    url='https://github.com/jdstemmler/pyadapt'
+)
