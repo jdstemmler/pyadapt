a63cc44fa9fdb6b678ce04cee919bb744f4f404f
Jayson Stemmler <jdstemmler@gmail.com>
2016-06-03 14:20:13 -0700

    allow for listing file attributes via function

---
+++ pyadapt/tools.py
@@ -0,0 +1,44 @@
+import os
+import re
+from .prettytable import PrettyTable, ALL
+from netCDF4 import Dataset
+
+
+def _check_variable_for_exclude(exclude, name):
+
+    return sum(len(re.findall(e, name)) for e in exclude) > 0
+
+
+def list_file_attributes(filename, exclude=None):
+
+    if not os.path.exists(filename):
+        raise FileNotFoundError("{} Not Found".format(filename))
+
+    if os.path.isdir(filename):
+        raise IsADirectoryError("{} is a Directory".format(filename))
+
+    if not os.path.isfile(filename):
+        raise FileNotFoundError("HOW??")
+
+    with Dataset(filename, 'r') as D:
+
+        p = PrettyTable(['Name', 'Units', 'Dimensions'])
+        p.align = 'l'
+        p.hrules = ALL
+
+        for name, variable in D.variables.items():
+
+            if (exclude is not None) & (isinstance(exclude, (list, tuple))):
+                if _check_variable_for_exclude(exclude, name):
+                    continue
+                else:
+                    pass
+            else:
+                pass
+
+            p.add_row(["{}\n  '{}'".format(getattr(variable, 'long_name', name).upper(), name),
+                       getattr(variable, 'units', None),
+                       getattr(variable, 'dimensions', None)
+                       ])
+
+        print(p.get_string(sortby="Name"))
